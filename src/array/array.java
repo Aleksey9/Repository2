6c8fee64c92719f8408cefcd4cb10a08cb8ef368
student <student@StudentPC>
2016-04-06 20:20:47 +0300

    rtyj

---
+++ src/array/array.java
@@ -0,0 +1,23 @@
+package array;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class array {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		Map<String, String> mp = new HashMap<String, String>();
+
+		for (int i = 0; i < N; i++) {
+			String c = in.next();
+			int lnt = c.length();
+			for (int k = 0; k < lnt; k++){
+				
+			}
+		}
+
+	}
+
+}
